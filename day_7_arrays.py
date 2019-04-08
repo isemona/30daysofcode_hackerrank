f96eaba56ac718e5dddb584944ecf934bb00d570
Semona Igama <semona.igama@gmail.com>
2019-04-07 23:48:44 -0700

    day_7_arrays

---
+++ day_7_arrays.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+
+    # write to handle to arry input
+
+    a = list(input().rstrip().split())
+    print(' '.join(reversed(a)))
+
+    
